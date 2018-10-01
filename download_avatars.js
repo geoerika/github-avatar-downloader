50e2e3fdbd808fbb32be2ada8b267ac9ebf9de06
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-01 23:47:33 +0000

    finished implementing the callback function to print out the list of avatar_urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,37 @@
+var secrets = require('./secrets.js');
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request.get(options, function(err, res, body) {
+
+    if (!err && res.statusCode == 200 ) {
+        var contributors = JSON.parse(body);
+        cb(err, contributors);
+    }
+    else {
+        console.log(err);
+    }
+  });
+}
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+
+  for (var contr of result) {
+    console.log("Errors:", err);
+    console.log("Result:", contr.avatar_url);
+  }
+});
+
