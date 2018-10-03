2a6d5db17b76c64c117900223fedebc4d0ef7834
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-03 12:36:30 +0000

    implemented getRepoContributors function to create a new directory and download avatars images in it

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var secrets = require('./secrets.js');
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -24,14 +25,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-
-
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log("Images downloaded!!");
+  })
+  .pipe(fs.createWriteStream(filePath))
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
 
+  fs.mkdir('./avatars/', function(err){
+      if (err) {
+      return console.error(err);
+      }
+  });
   for (var contr of result) {
-    console.log("Errors:", err);
-    console.log("Result:", contr.avatar_url);
+    var filePath = "";
+    filePath = './avatars/' + contr.login + '.jpg';
+    downloadImageByURL(contr.avatar_url, filePath);
   }
 });
 
